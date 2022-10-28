10122b3978d9369d96de2a370dd5bc501ee71e56
KaleoMello7 <108874861+KaleoMello7@users.noreply.github.com>
2022-10-28 10:54:36 -0300

    java

---
+++ TestIfElse.java
@@ -0,0 +1,16 @@
+public class TestIfElse {
+
+    public static void main(String[] args) {
+      int constante = 76;
+    
+    char nome;
+    if (constante >=90) {
+      nome = 'A';
+    } else if (constante >=80) {
+      nome = 'B';
+    } else if (constante >= 70) {
+      nome = 'C';
+    } else if (constante >= 60) {
+      nome = 'D';
+
+}
